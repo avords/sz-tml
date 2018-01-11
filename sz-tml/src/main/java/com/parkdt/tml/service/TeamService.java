0624ec9fd81cb55ffc38237444e4b576ad0c6e35
yandongquan <yandongquan@lvmama.com>
2018-01-11 10:01:01 +0800

    增加团队Service

---
+++ sz-tml/src/main/java/com/parkdt/tml/service/TeamService.java
@@ -0,0 +1,12 @@
+package com.parkdt.tml.service;
+
+import com.parkdt.tml.domain.TeamBasicInformation;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/1/11.
+ */
+public interface TeamService {
+    List<TeamBasicInformation> getAllTeam();
+}
---
+++ sz-tml/src/main/java/com/parkdt/tml/service/impl/TeamServiceImpl.java
@@ -0,0 +1,16 @@
+package com.parkdt.tml.service.impl;
+
+import com.parkdt.tml.domain.TeamBasicInformation;
+import com.parkdt.tml.service.TeamService;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/1/11.
+ */
+public class TeamServiceImpl implements TeamService {
+    @Override
+    public List<TeamBasicInformation> getAllTeam() {
+        return null;
+    }
+}
